5cb12f5543377e1d5b4f1f284b8916ba34910e74
Luuka Lindgren <luslin@utu.fi>
2021-12-21 11:20:35 +0200

    hakupalkki historialle tehty

--- app.js
+++
@@ -1,9 +0,0 @@
-router.get('/', function (req, res, next) {
-
-    axios.get('https://bad-api-assignment.reaktor.com/rps/history/')
-    .then(response => {
-        res.send(response.data.result);
-    }).catch(error => {
-        res.send(error.message);
-    })
-});
